6a99be181185a56e477e4b9eacff83447c3314ff
Christian Jodice <mrjodice@gmail.com>
2025-04-12 02:13:44 -0400

    scopeLab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+// Global variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a new burger
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Block-scoped variable in if statement
+if(true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Function to change the featured drink
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
